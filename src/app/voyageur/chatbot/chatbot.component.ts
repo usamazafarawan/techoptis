851b78ded71191f1510c1758dbe977a117353b75
usamazafarawan <108418887+UsamaZaffar@users.noreply.github.com>
2024-06-22 01:49:30 +0500

    chat bot work

--- src/app/voyageur/chatbot/chatbot.component.ts
+++ src/app/voyageur/chatbot/chatbot.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { ChatService, Message } from '../../chat.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chatbot',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './chatbot.component.html',
   styleUrl: './chatbot.component.css'
 })
 export class ChatbotComponent {
+
+  messages: Message[] = [];
+  value: string='';
+
+  constructor(public chatService: ChatService) { }
+
+  
+  ngOnInit() {
+    this.chatService.conversation.subscribe((val:any) => {
+    this.messages = this.messages.concat(val);
+  });
+}
+
+sendMessage() {
+  this.chatService.getBotAnswer(this.value);
+  this.value = '';
+}
 
 }
