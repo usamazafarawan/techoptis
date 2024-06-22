5119b9f847be1ae9a97ef0031b81a8db7eea6ff8
mubich1 <mubshirnaseer24@gmail.com>
2024-06-22 13:48:23 +0500

    stripe inte

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,10 +6,10 @@ import { provideToastr } from 'ngx-toastr';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
+import { NgxStripeModule } from 'ngx-stripe';
 export const appConfig = {
   providers: [
-    importProvidersFrom(BrowserModule),
+    importProvidersFrom(BrowserModule,NgxStripeModule.forRoot('pk_test_51PUN6OIcTsPq2p4JphmTadz9Re7mh2wR0cCaMMiPGOi4iHzwBhSqeUu0uikbiEdNDAvtXUDPf8a6mwT6XYBKcUaV00J9cOe7Xk')),
     provideRouter(routes),
     provideToastr({positionClass: 'toast-top-right'}),
     provideAnimations(),
--- src/app/voyageur/facture-paiement/facture-paiement.component.ts
+++ src/app/voyageur/facture-paiement/facture-paiement.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+// import { StripeElementsOptions } from '@stripe/stripe-js';
+import { NgxStripeModule, StripeService} from 'ngx-stripe';
+import { StripeCardComponent } from 'ngx-stripe';
 @Component({
   selector: 'app-facture-paiement',
   standalone: true,
-  imports: [],
+  imports: [NgxStripeModule],
   templateUrl: './facture-paiement.component.html',
   styleUrl: './facture-paiement.component.css'
 })
 export class FacturePaiementComponent {
+  @ViewChild('cardComponent', { static: true }) cardComponent!: ElementRef;
 
+  elementsOptions:any = {
+    locale: 'en'
+  };
+
+  cardOptions:any = {
+    style: {
+      base: {
+        iconColor: '#666EE8',
+        color: '#31325F',
+        fontWeight: '300',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSize: '18px',
+        '::placeholder': {
+          color: '#CFD7E0'
+        }
+      }
+    }
+  };
+
+  constructor(private stripeService: StripeService) {}
+
+  handlePayment() {
+    this.stripeService.createToken(this.cardComponent.nativeElement, {})
+      .subscribe((result) => {
+        if (result.token) {
+          // Handle the Stripe token
+          console.log(result.token.id);
+        } else if (result.error) {
+          // Handle any errors
+          console.log(result.error.message);
+        }
+      });
+  }
 }
+
